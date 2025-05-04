81f71571cbd848ee218746e3f3636233a0e97b62
Kristofers Solo <dev@kristofers.xyz>
2025-05-04 14:49:49 +0300

    refactor(ui): separate to dir

--- src/grovers_visualizer/ui/__init__.py
+++ src/grovers_visualizer/ui/__init__.py
@@ -1,6 +1,6 @@
 from importlib.util import find_spec
 
-from .parse import Args
+from grovers_visualizer.parse import Args
 
 
 def is_dearpygui_available() -> bool:
@@ -14,3 +14,7 @@ def run_dpg_ui(_args: Args) -> None:
     if not is_dearpygui_available():
         print("DearPyGui is not installed. Install with: pip install .[ui]")
         return
+
+    from .dpg import run_dearpygui_ui
+
+    run_dearpygui_ui()
